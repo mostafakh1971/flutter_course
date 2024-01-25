6fcaab871be34f2bfba78a2c790ff3518ed5aaad
mostafakh1971 <mostafa.kartabeel@gmail.com>
2024-01-25 02:18:40 -0800

    stack

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/home_screen.dart';
+import 'package:flutter_course/second_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,60 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Course Two"),
-            backgroundColor: Colors.blue,
-            foregroundColor: Colors.white,
-          ),
-          body: Container(
-              width: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // 1 + 1 + 1 + 1 = 4
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      color: Colors.red,
-                      child: Text(
-                        "First Text",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 3,
-                    child: Container(
-                      color: Colors.amber,
-                      child: Text(
-                        "Second Text",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.blue,
-                      child: Text(
-                        "Third Text",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.green,
-                      child: Text(
-                        "Fourth Text",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  )
-                ],
-              )),
-        ));
+      debugShowCheckedModeBanner: false,
+      home: SecondScreen(),
+    );
   }
 }
